f6a4dd6e5222ab22dea81ca0a71826e3bf6c7e60
AnaComBia <anabeatrizetorres@gmail.com>
2021-09-03 16:42:31 -0300

    finaliza programa To Nervosa

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ToNervosa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine($"usuario, qual é a sua opinião sobre mim? Porquê eu te acho bem xoxo capenga ");
+            string OpiniaoUser = Console.ReadLine();
+        
+            Console.WriteLine($"\n{OpiniaoUser.Remove(10)}...esquece, \nto nervosa");
+
+        }
+
+    }
+}
